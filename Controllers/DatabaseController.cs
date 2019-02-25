f0731265166f1eb557a68a28e74ff88f9ea91d96
PatrykBob <patryk.xperia,s@gmail.com>
2019-02-25 20:57:58 +0100

    Patryk
    Przeniesienie obsługi bazy do DatabaseController

---
+++ Controllers/DatabaseController.cs
@@ -0,0 +1,24 @@
+﻿using CzytnikRSS.Models;
+using LiteDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CzytnikRSS.Controllers
+{
+    public class DatabaseController : Controller
+    {
+        // GET: Database
+        public void ZapiszDoBazy(Site site)
+        {
+            using (var db = new LiteDatabase(@"D:\dane.db"))
+            {
+                var col = db.GetCollection<Site>("sites");
+
+                col.Insert(site);               
+            }
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,15 +1,33 @@
-﻿using System;
+﻿using CzytnikRSS.Models;
+using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml.Linq;
 
 namespace CzytnikRSS.Controllers
 {
     public class HomeController : Controller
     {
+        DatabaseController dbController = new DatabaseController();
         public ActionResult Index()
         {
+            List<Site> lista = new List<Site>();
+            XElement rss = XElement.Load("http://windows.wvista.pl/rss.xml");
+            
+                foreach (var item in rss.Descendants("item"))
+                {
+                    var site = new Site
+                    {
+                        title = item.Element("title").Value,
+                        description = item.Element("description").Value,
+                        pubDate = DateTime.Now
+                    };
+                    dbController.ZapiszDoBazy(site);
+                }
+
             return View();
         }
 
