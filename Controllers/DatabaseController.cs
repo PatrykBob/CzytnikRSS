d363612a928a2ae9bc57d511d23cc31fb8741382
Patryk <patryk.xperia.s@gmail.com>
2019-03-05 20:02:43 +0100

    Zapisywanie linków w bazie

--- Controllers/DatabaseController.cs
+++ Controllers/DatabaseController.cs
@@ -10,11 +10,19 @@ namespace CzytnikRSS.Controllers
 {
     public class DatabaseController : Controller
     {
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).ToString() + "/dane.db";
+        public void ZapiszLinkDoBazy(Source link)
+        {
+            using (var db = new LiteDatabase(path))
+            {
+                var col = db.GetCollection<Source>("links");
+
+                col.Insert(link);
+            }
+        }
         // GET: Database
         public void ZapiszStroneDoBazy(Site site)
         {
-            string path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData).ToString();
-            path += "/dane.db";
             using (var db = new LiteDatabase(path))
             {
                 var col = db.GetCollection<Site>("sites");
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 ﻿using CzytnikRSS.Models;
+using HtmlAgilityPack;
 using LiteDB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using System.Xml.Linq;
@@ -15,20 +17,58 @@ namespace CzytnikRSS.Controllers
         public ActionResult Index()
         {
             List<Site> lista = new List<Site>();
-            XElement rss = XElement.Load("http://windows.wvista.pl/rss.xml");
-            
-                foreach (var item in rss.Descendants("item"))
-                {
-                    var site = new Site
-                    {
-                        title = item.Element("title").Value,
-                        description = item.Element("description").Value,
-                        pubDate = DateTime.Now
-                    };
-                    dbController.ZapiszStroneDoBazy(site);
-                }
+
+            PobierzLinkiStron();
 
             return View();
+        }
+
+        public void PobierzLinkiStron()
+        {
+            string link = "http://www.rss.lostsite.pl/index.php?rss=32";
+
+            using (var client = new WebClient())
+            {
+                string html = client.DownloadString(link);
+
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(html);
+                List<HtmlAttribute> lista = new List<HtmlAttribute>();
+                foreach(HtmlNode lin in doc.DocumentNode.SelectNodes("//a[@href]"))
+                {
+                    //HtmlAttribute href = link.Attributes["href"];
+                    HtmlAttribute rel = lin.Attributes["rel"];
+                    HtmlAttribute id = lin.Attributes["id"]; 
+                    if (rel != null && id == null)
+                    {
+                        if (lin.Attributes["rel"].Value == "nofollow")
+                        {
+                            Source l = new Source()
+                            {
+                                name = "test",
+                                link = lin.Attributes["href"].Value
+                            };
+                            dbController.ZapiszLinkDoBazy(l);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void PobierzElementyZeStrony (string link)
+        {
+            XElement rss = XElement.Load(link);
+
+            foreach (var item in rss.Descendants("item"))
+            {
+                var site = new Site
+                {
+                    title = item.Element("title").Value,
+                    description = item.Element("description").Value,
+                    pubDate = DateTime.Now
+                };
+                dbController.ZapiszStroneDoBazy(site);
+            }
         }
 
         public ActionResult About()
